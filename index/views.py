f6d9f3d15b58d36d45118617e1d308ae41ee3dd0
qurg <qurg@outlook.com>
2019-04-05 00:12:33 +0800

    change db

--- index/views.py
+++ index/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from index.models import Product
+
+
+def index(request):
+    type_list = Product.objects.values('type').distinct()
+    name_list = Product.objects.values('name', 'type')
+    return render(request, 'index.html', locals())
+
+
+class ProductList(ListView):
+    context_object_name = 'type_list'
+
+    template_name = 'index.html'
+
+    queryset = Product.objects.values('type').distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name_list'] = Product.objects.values('name', 'type')
+        return context
--- star/settings.py
+++ star/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'star',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
